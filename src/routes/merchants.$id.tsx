b14f7567a8fb911c0e8145bcc35b1097cac0a6ed
lettusspray <bentlysandthecaravan@gmail.com>
2026-09-06 14:52:42 +0100

    Add merchant detail page with tabs

---
+++ src/routes/merchants.$id.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+import { MerchantDetailPage } from "@/components/merchants/merchant-detail-page";
+
+export const Route = createFileRoute("/merchants/$id")({
+  component: MerchantDetailRoute,
+});
+
+function MerchantDetailRoute() {
+  const { id } = Route.useParams();
+  return <MerchantDetailPage merchantId={id} />;
+}
